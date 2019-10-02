497862e5f14546e4d607ac02bd8a29d3f032785b
Justice Reed <justice@amper.xyz>
2019-10-02 10:13:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ var network_list;
 
 // document elements for UI
 var scanButton=document.getElementById('scan-button');
+var manualButton=document.getElementById('manual-button');
 var initialConnectButton=document.getElementById('initial-connect-button');
 var connectButton=document.getElementById('connect-button');
 var connectFeedback=document.getElementById('connect-feedback');
@@ -21,7 +22,24 @@ var copyButton=document.getElementById('copy-button');
 var showButton=document.getElementById('show-button');
 var deviceID=document.getElementById('device-id');
 var connectForm=document.getElementById('connect-form');
+var connectDiv = document.getElementById('connect-div');
 var scanDiv = document.getElementById('scan-div');
+var manualDiv = document.getElementById('manual-div');
+var manualForm = document.getElementById('manual-form');
+var networksDiv = document.getElementById('networks-div');
+var networkInfoDiv = document.getElementById('networks-info-div');
+var encryptionRadioDiv = document.getElementById('encryption-radio');
+var securityRadioDiv = document.getElementById('security-radio');
+
+
+const    SECURITY_OPEN           = 0;          /**< Unsecured                               */
+const    SECURITY_WEP_PSK        = 1;     	  /**< WEP Security with open authentication   */
+const    SECURITY_WEP_SHARED     = 0x8001;     /**< WEP Security with shared authentication */
+const    SECURITY_WPA_TKIP_PSK   = 0x00200002; /**< WPA Security with TKIP                  */
+const    SECURITY_WPA_AES_PSK    = 0x00200004; /**< WPA Security with AES                   */
+const    SECURITY_WPA2_AES_PSK   = 0x00400004; /**< WPA2 Security with AES                  */
+const    SECURITY_WPA2_TKIP_PSK  = 0x00400002; /**< WPA2 Security with TKIP                 */
+const    SECURITY_WPA2_MIXED_PSK = 0x00400006; /**< WPA2 Security with AES & TKIP           */
 
 // ----------------------------------------------------------------------------------------------------------------
 // these wrap get and post requests in the proper format for a Photon to parse.
@@ -62,19 +80,20 @@ var postRequest=function(request_url, request_body, request_callback){
 // this is a pre-requisite for being able to send a WiFi password within the WiFi config object
 var public_key_callback={
     success:function(a){
+        initialConnectButton.innerHTML = 'Connect To Gateway'
         console.log('Public key: '+a.b);
         public_key=a.b;
         rsa.setPublic(public_key.substring(58, 58+256), public_key.substring(318, 318+6));
-        initialConnectButton.innerHTML = 'Connect To Device'
         initialConnectButton.disabled = false;
         connectFeedback.innerHTML = 'Connection was successful!';
         enableButtons();
         scanDiv.style.display='block';
+        manualDiv.style.display='block';
     }, 
     error:function(a, b){
         console.log(a);
         resetPage();
-        connectFeedback.innerHTML = '<b>ERROR - Could not connect to Device, please retry.</b><br><br>Please make sure Device is in listening mode, and that you are connected to the PHOTON-XXXX wifi hotspot corresponding to the device.<br><br>  If you are connected to the hotspot, try turning your WiFi off and back on again.';
+        connectFeedback.innerHTML = '<b>ERROR - Could not connect to Gateway, please retry.</b><br><br>Please make sure Gateway LED is white, and that you are connected to the AMPER-XXX wifi hotspot corresponding to the device.<br><br>  If you are connected to the hotspot, try turning your WiFi off and back on again.';
 }};
 
 var getDevicePublicKey = function(){
@@ -83,10 +102,13 @@ var getDevicePublicKey = function(){
     initialConnectButton.innerHTML = '&nbsp&nbsp&nbsp Connecting... &nbsp&nbsp&nbsp';
     initialConnectButton.disabled = true;
     scanDiv.style.display='none';
-    document.getElementById('connect-div').style.display='none';
-    document.getElementById('networks-div').innerHTML = '';
+    networkInfoDiv.style.display='none';
+    connectDiv.style.display='none';
+    manualDiv.style.display='none';
+    networksDiv.innerHTML = '';
     getRequest(base_url+'public-key', public_key_callback);
 }
+
 
 // ----------------------------------------------------------------------------------------------------------------
 // This request gets a list of wifi networks and signal strength values
@@ -94,25 +116,29 @@ var getDevicePublicKey = function(){
 // this negates the need to select WiFi network config like encryption type
 var scan_callback={
     success:function(a){
+        networkInfoDiv.style.display='block';
         network_list=a.scans;
         console.log('I found:');
-        var b=document.getElementById('networks-div');
-        b.innerHTML='';
-        if(network_list.length>0) for(var c=0; c<network_list.length; c++){
-        ssid=network_list[c].ssid;
-        console.log(network_list[c]);
-        add_wifi_option(b, ssid);
-        document.getElementById('connect-div').style.display='block';
-        }else b.innerHTML='<p class=\'scanning-error\'>No networks found.</p>';
+        networksDiv.innerHTML='<h4>Available Networks</h4>';
+        if(network_list.length>0) {
+            connectDiv.style.display='block';
+            manualForm.style.display='none';
+            for(var c=0; c<network_list.length; c++){
+                ssid=network_list[c].ssid;
+                console.log(network_list[c]);
+                add_radio_option(networksDiv, "ssid", ssid);
+            }
+        }
+        else networksDiv.innerHTML='<p class=\'scanning-error\'>No networks found.</p>';
     }, 
     error:function(a){
         console.log('Scanning error:'+a);
-        document.getElementById('networks-div').innerHTML='<p class=\'scanning-error\'>Scanning error.</p>';
+        networksDiv.innerHTML='<p class=\'scanning-error\'>Scanning error.</p>';
     }, 
     regardless:function(){
         scanButton.innerHTML='Re-Scan';
         enableButtons();
-        document.getElementById('networks-div').style.display='block';
+        networksDiv.style.display='block';
 }};
 
 var scan=function(){
@@ -120,8 +146,9 @@ var scan=function(){
     disableButtons();
     scanButton.innerHTML='Scanning...';
     connectButton.innerHTML='Connect';
-    document.getElementById('connect-div').style.display='none';
-    document.getElementById('networks-div').style.display='none';
+    connectDiv.style.display='none';
+    manualForm.style.display='none';
+    networksDiv.style.display='none';
     getRequest(base_url+'scan-ap', scan_callback);
 };
 
@@ -133,7 +160,7 @@ var configure_callback={
         console.log('Credentials received.');
         connectButton.innerHTML='Credentials received... Resetting Device';
         reset();
-        connectFeedback.innerHTML="New WiFi credentials received.<br><br>The device will reset and attempt to connect. <br><br>If the Device enters listening mode again, the credentials could not be used to successfully connect and you will need to retry";
+        connectFeedback.innerHTML="New WiFi credentials received.<br><br>The device will reset and attempt to connect.  You should see the Gateway LED turn Green or Yellow within a minute.<br><br>If the LED turns White again, the credentials could not be used to successfully connect and you will need to retry";
         resetPage();
     }, 
     error:function(a, b){
@@ -145,18 +172,54 @@ var configure_callback={
 
 var configure=function(a){
     a.preventDefault();
-    var b=get_selected_network();
-    var c=document.getElementById('password').value;
-    if(!b){
-        window.alert('Please select a network!');
-        return false;
+    var request_payload;
+    if (manualForm.style.display=='none') {
+        var b=get_selected_network();
+        if(!b){
+            window.alert('Please select a network!');
+            return false;
+        }
+        var sel_pw=document.getElementById('password').value;
+        console.log(sel_pw)
+        request_payload={idx:0, ssid:b.ssid, pwd:rsa.encrypt(sel_pw), sec:b.sec, ch:b.ch};
     }
-    var d={idx:0, ssid:b.ssid, pwd:rsa.encrypt(c), sec:b.sec, ch:b.ch};
+    else {
+        var sel_ssid=document.getElementById('ssid').value;
+        var sel_pw=document.getElementById('password').value;
+        var sel_security = getRadioVal(this, 'security');
+        sel_security = sel_security ? sel_security : "";
+        var sel_encryption = getRadioVal(this, 'encryption');
+        sel_encryption = sel_encryption ? sel_encryption : "";
+        console.log(sel_security);
+        console.log(sel_encryption);
+        var sel_sec = convert_selections_to_security(sel_security, sel_encryption);
+        console.log(sel_sec);
+        if(!sel_ssid || sel_sec == null || (!sel_pw && sel_security != "OPEN")){
+            window.alert('Please provide all necessary credentials!');
+            return false;
+        }
+        request_payload={idx:0, ssid:sel_ssid, pwd:rsa.encrypt(sel_pw), sec:sel_sec, ch:0};
+    }
     connectButton.innerHTML='Sending credentials...';
     disableButtons();
-    console.log('Sending credentials: '+JSON.stringify(d));
-    postRequest(base_url+'configure-ap', d, configure_callback);
+    console.log('Sending credentials: '+JSON.stringify(request_payload));
+    postRequest(base_url+'configure-ap', request_payload, configure_callback);
 };
+
+
+var handle_manual_button=function(){
+    scanDiv.style.display = 'block';
+    connectDiv.style.display = 'block';
+    manualForm.style.display = 'block';
+    networksDiv.style.display = 'none';
+    scanButton.innerHTML='Scan';
+    networkInfoDiv.style.display='none'
+    securityRadioDiv.innerHTML = '<p>Security Type</p>';
+    add_radio_option(securityRadioDiv, "security", "OPEN");
+    add_radio_option(securityRadioDiv, "security", "WEP");
+    add_radio_option(securityRadioDiv, "security", "WPA");
+    add_radio_option(securityRadioDiv, "security", "WPA2");
+}
 
 // ----------------------------------------------------------------------------------------------------------------
 // this calls a special function to reset the device, which will subsequently attempt a connection
@@ -186,28 +249,129 @@ var enableButtons=function(){
     scanButton.disabled=false;
 };
 
-var add_wifi_option=function(a, b){
-    var c=document.createElement('INPUT');
-    c.type='radio';
-    c.value=b;
-    c.id=b;
-    c.name='ssid';
-    c.className='radio';
-    var d=document.createElement('DIV');
-    d.className='radio-div';
-    d.appendChild(c);
-    var e=document.createElement('label');
-    e.htmlFor=b;
-    e.innerHTML=b;
-    d.appendChild(e);
-    a.appendChild(d);
+var add_radio_option=function(wrapper_div, type_name, element_name){
+    var input_element=document.createElement('INPUT');
+    input_element.type='radio';
+    input_element.value=element_name;
+    input_element.id=element_name;
+    input_element.name=type_name;
+    input_element.className='radio';
+    var radio_div=document.createElement('DIV');
+    radio_div.className='radio-div';
+    radio_div.appendChild(input_element);
+    var label_element=document.createElement('label');
+    label_element.htmlFor=element_name;
+    label_element.innerHTML=element_name;
+    radio_div.appendChild(label_element);
+    wrapper_div.appendChild(radio_div);
 };
+
 
 var get_selected_network=function(){
     for(var a=0; a<network_list.length; a++){
     ssid=network_list[a].ssid;
     if(document.getElementById(ssid).checked) return network_list[a];
 }};
+
+var get_encryption_options_from_security=function(security){
+    if (security == "OPEN") return [];
+    else if (security == "WEP") return ["PSK", "SHARED"];
+    else if (security == "WPA") return ["TKIP", "AES"];
+    else return ["TKIP", "AES", "AES/TKIP"];
+}
+
+var convert_security_to_selections=function(sec_val) {
+    switch(sec_val) {
+        case SECURITY_OPEN: {
+            return ("OPEN", "");
+        }
+        case SECURITY_WEP_PSK: {
+            return ("WEP", "PSK");
+        }
+        case SECURITY_WEP_SHARED: {
+            return ("WEP", "SHARED");
+        }
+        case SECURITY_WPA_TKIP_PSK: {
+            return ("WPA", "TKIP");
+        }
+        case SECURITY_WPA_AES_PSK: {
+            return ("WPA", "AES");
+        }
+        case SECURITY_WPA2_TKIP_PSK: {
+            return ("WPA2", "TKIP");
+        }
+        case SECURITY_WPA2_AES_PSK: {
+            return ("WPA2", "AES");
+        }
+        case SECURITY_WPA2_MIXED_PSK: {
+            return ("WPA2", "AES/TKIP");
+        }
+        default: {
+            return null;
+        }
+    }
+}
+
+var convert_selections_to_security=function(security, encryption) {
+    if (security == "OPEN") return SECURITY_OPEN;
+    else if (security == "WEP") {
+        if (encryption == "PSK") return SECURITY_WEP_PSK;
+        else if (encryption == "SHARED") return SECURITY_WEP_SHARED;
+    }
+    else if (security == "WPA") {
+        if (encryption == "TKIP") return SECURITY_WPA_TKIP_PSK;
+        else if (encryption == "AES") return SECURITY_WPA_AES_PSK;
+    }
+    else {
+        if (encryption == "TKIP") return SECURITY_WPA2_TKIP_PSK;
+        else if (encryption == "AES") return SECURITY_WPA2_AES_PSK;
+        else if (encryption == "AES/TKIP") return SECURITY_WPA2_MIXED_PSK;
+    }
+    return null;
+}
+
+var update_encryption_options=function(){
+    if (document.getElementById("OPEN").checked) {
+        encryptionRadioDiv.innerHTML = '';
+    }
+    else {
+        encryptionRadioDiv.innerHTML = '<p>Encryption Type</p>';
+        if (document.getElementById("WEP").checked) {
+            var selections = get_encryption_options_from_security("WEP");
+            for (var i = 0; i < selections.length; i++) {
+                add_radio_option(encryptionRadioDiv, "encryption", selections[i]);
+            }
+        }
+        else if (document.getElementById("WPA").checked) {
+            var selections = get_encryption_options_from_security("WPA");
+            for (var i = 0; i < selections.length; i++) {
+                add_radio_option(encryptionRadioDiv, "encryption", selections[i]);
+            }
+        }
+        else if (document.getElementById("WPA2").checked) {
+            var selections = get_encryption_options_from_security("WPA2");
+            for (var i = 0; i < selections.length; i++) {
+                add_radio_option(encryptionRadioDiv, "encryption", selections[i]);
+            }
+        }
+    }
+}
+
+function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+    
+    if (!radios) return null;
+    // loop through list of radio buttons
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { // radio checked?
+            val = radios[i].value; // if so, hold its value in val
+            break; // and break out of for loop
+        }
+    }
+    return val; // return value of checked radio or undefined if none checked
+}
 
 var toggleShow=function(){
     var a=document.getElementById('password');
@@ -225,11 +389,13 @@ var resetPage = function(){
     rsa=new RSAKey();
     public_key = "";
     scanButton.innerHTML='Scan';
-    initialConnectButton.innerHTML = 'Connect To Device'
+    networkInfoDiv.style.display='none'
+    initialConnectButton.innerHTML = 'Connect To Gateway'
     initialConnectButton.disabled = false;
     scanDiv.style.display='none';
-    document.getElementById('connect-div').style.display='none';
-    document.getElementById('networks-div').innerHTML = '';
+    manualDiv.style.display='none';
+    connectDiv.style.display='none';
+    networksDiv.innerHTML = '';
     enableButtons();
 }
 
@@ -239,12 +405,17 @@ if(scanButton.addEventListener){
     initialConnectButton.addEventListener('click',  getDevicePublicKey);
     showButton.addEventListener('click', toggleShow);
     scanButton.addEventListener('click', scan);
+    manualButton.addEventListener('click', handle_manual_button);
+    securityRadioDiv.addEventListener('click', update_encryption_options);
     connectForm.addEventListener('submit', configure);
+    
 }
 else if(scanButton.attachEvent){
     initialConnectButton.attachEvent('onclick',  getDevicePublicKey);
     showButton.attachEvent('onclick', toggleShow);
     scanButton.attachEvent('onclick', scan);
+    manualButton.addEvent('onclick', handle_manual_button);
+    securityRadioDiv.addEvent('onclick', update_encryption_options);
     connectForm.attachEvent('onsubmit', configure);
 }
 
